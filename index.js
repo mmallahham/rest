e725e673c7244f6ffb79962ccf07f8a1199b544e
mmallahham <mmallahham@hotmail.com>
2018-01-03 15:06:04 +0100

    restructured

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
 var express = require('express');
+var mongoose = require('mongoose');
+var Book = require('./modules/bookmodel.js');
+
+
+mongoose.connect('mongodb://localhost/test');
 
 var app = express();
 
 var PORT = process.env.PORT || 5000;
 
-app.get('/',(req,res)=>{
+var bookRouter = express.Router();
+
+bookRouter.route('/books')
+    .get(function(req,res){
+        Book.find(function(err,books){
+            if(err)
+                res.status(500).send(err);
+            else
+                console.log(books);
+                res.json(books);
+        });
+      //  res.json({Name :'my first REST app'});
+    });
+
+
+app.use('/',bookRouter);
+
+app.get('/',function(req,res){
     res.send('Welcome son!');
     console.log('the root was visited!!!!');
 });
 
-app.listen(PORT,()=>{
+app.listen(PORT,function(){
     console.log('listening on port ' + PORT);
 });
