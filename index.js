d31a09123161154ecee92326ea873ca03fb794d0
mmallahham <mmallahham@hotmail.com>
2018-01-03 15:45:28 +0100

    add body-parser

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var Book = require('./modules/bookmodel.js');
+var bodyParser = require('body-parser');
 
 
 mongoose.connect('mongodb://localhost/test');
@@ -10,6 +11,9 @@ var app = express();
 var PORT = process.env.PORT || 5000;
 
 var bookRouter = express.Router();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
 bookRouter.route('/books')
     .get(function(req,res){
@@ -21,6 +25,30 @@ bookRouter.route('/books')
                 res.json(books);
         });
       //  res.json({Name :'my first REST app'});
+    });
+    
+
+    bookRouter.route('/books/:bookId')
+    .get(function(req,res){
+        var bookId = req.params.bookId;
+        Book.find({_id:bookId},function(err,books){
+            if(err)
+                res.status(500).send(err);
+            else
+                console.log(bookId);
+                res.json(books);
+        });
+      //  res.json({Name :'my first REST app'});
+    })
+    .post(function(req,res){
+        var bookId = req.params.bookId;
+        Book.find({_id:bookId},function(err,books){
+            if(err)
+                res.status(500).send(err);
+            else
+                console.log(req.body);
+            res.json(books);    
+        });
     });
 
 
